067e877eb28092439ca594495a36726698abe7dd
Kathleen <kathleen.wilsonn@gmail.com>
2014-11-03 13:20:46 -0500

    Left Menu for Order List
    Left search html template menu for operators order list

---
+++ Server/web/operator/operatorLeftMenu.php
@@ -0,0 +1,21 @@
+	<div id="left">
+		<h3>Options</h3>
+		<p class="auto-style2">
+		Show the most recent 
+		<input name="txtRecords" style="width: 27px" type="text" />records</p>
+		<p class="auto-style2">
+		<input name="ckAllOrders" type="checkbox" />Show all Orders</p>
+		<p class="auto-style2">
+		<input name="ckActiveOrders" type="checkbox" />Show all Active Orders</p>
+		<p class="auto-style2">
+		<input name="ckActiveOrders0" type="checkbox" />Show all Completed Orders</p>
+		<p class="auto-style2">
+		<input name="ckActiveOrders1" type="checkbox" />Show all Canceled Orders</p>
+		<p class="auto-style3">
+		<button name="btnSearch" >
+		<div >	Search </div>
+		</button>
+		&nbsp;
+		</p>
+		<br />
+	</div>
