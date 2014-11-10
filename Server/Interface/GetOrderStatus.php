a97f5c1288218555d1cc26a26cd169edf9020417
Woo Seung Sim <victor.ws.sim@gmail.com>
2014-11-10 00:30:13 -0500

    Closes #58 Implements order status handler

---
+++ Server/Interface/GetOrderStatus.php
@@ -0,0 +1,30 @@
+<?php
+
+$user_input = empty($_POST)?$_GET:$_POST;
+$table = "orders";
+$cid = $user_input['orderid'];
+
+
+//connect to database
+mysql_connect("localhost", "root", "wechao") or
+  die("Could not connect: " . mysql_error());
+
+//select a database
+mysql_select_db("foodorder");
+
+//select orderId
+$result = mysql_query("SELECT * FROM $table WHERE idorder=$cid");
+
+$order = array(
+            "result"=>""
+        );
+$row = mysql_fetch_array($result);
+$order["result"] = $row["status"];
+
+
+
+$output = json_encode($order);
+print_r($output);
+
+
+?>
