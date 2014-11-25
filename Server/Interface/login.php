58bb52b91687fc706653d7d4b955c747db3c23c3
Woo Seung Sim <victor.ws.sim@gmail.com>
2014-11-25 13:20:51 -0500

    Change encryption method to MD5 from SHA1

--- Server/Interface/login.php
+++ Server/Interface/login.php
@@ -5,7 +5,7 @@ $name = $user_input['name'];
 $pwd = $user_input['pwd'];
 //echo "order id is $orderId<br>";
 
-$result = mysql_query("SELECT * FROM $table where (name='$name' AND pwd=SHA1('$pwd'))");
+$result = mysql_query("SELECT * FROM $table where (name='$name' AND pwd=MD5('$pwd'))");
 
 $user = array(
         "userid"=>"",
