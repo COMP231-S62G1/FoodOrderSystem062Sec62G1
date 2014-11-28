be05603f29914117f26cffac19638db46dc10c5f
alex <alexliubo@gmail.com>
2014-11-27 22:00:15 -0500

    debugged and deployed on server
    #123

--- Server/Interface/GetBalance.php
+++ Server/Interface/GetBalance.php
@@ -5,11 +5,9 @@ $userid = $user_input['userid'];
 //echo "order id is $orderId<br>";
 
 $result = mysql_query("SELECT balance FROM $table where userid='$userid')");
-
 $user = array(
         "balance"=>""
     );
-
 if($row = mysql_fetch_array($result)) {
     $user['balance'] = $row['balance'];
     $output = json_encode($user);
