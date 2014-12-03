c1abd027fab970c0e78c8ef571542c0698d8eb73
Woo Seung Sim <victor.ws.sim@gmail.com>
2014-12-02 21:25:00 -0500

    upload missing file

---
+++ Server/manager/storeheader.php
@@ -0,0 +1,31 @@
+<form action="storeaccount.php" method="POST" name="storeaccount">
+</form>
+<form action="storeinfo.php" method="POST" name="storeinfo">
+</form>
+<form action="storemenu.php" method="POST" name="storemenu">
+</form>
+<form action="storeorderreport.php" method="POST" name="storeorderreport">
+</form>
+<form action="storesalesreport.php" method="POST" name="storesalesreport"> 
+</form>
+<form action="storeinfo.php" method="POST" name="home">  
+</form>
+<form action="../logout.php" method="POST" name="logout">
+</form>
+<!-- header begins -->
+<div id="header">
+  <div id="menu">
+		<ul>
+			<li><a href="#" title="" onclick="document.home.submit();">Home</a></li>
+            <li><a href="#" title="" onclick="document.storeinfo.submit();">Store Information</a></li>
+			<li><a href="#" title="" onclick="document.storemenu.submit();">Menus</a></li>
+			<li><a href="#" title="" onclick="document.storeaccount.submit();">Account</a></li>
+			<li><a href="#" title="" onclick="document.storesalesreport.submit();">Report</a></li>
+			<li><a href="#" title="" onclick="document.logout.submit();">Logout</a></li>
+		</ul>
+	</div>
+	<div id="logo">
+		<h1><a href="http://www.centennialcollege.ca" title="Centennial College Food Order">Centennial College Food Order</a></h1>
+		<h2><a href="http://github.com/COMP231-S62G1/FoodOrderSystem062Sec62G1/" id="metamorph">Designed by Group 1</a></h2>
+	</div>
+</div>
