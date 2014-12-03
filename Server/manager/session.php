c2fee1ca1f800c20900589f1c54b7fed51ffed34
Woo Seung Sim <victor.ws.sim@gmail.com>
2014-12-03 12:53:34 -0500

    Create Session manager for login and logout Close #144   Logout functionality

---
+++ Server/manager/session.php
@@ -0,0 +1,22 @@
+<?php
+    include '../dbConnector.php';
+    // Establishing Connection with Server by passing server_name, user_id and password as a parameter
+    
+    $table = "operator";
+    
+    session_start();// Starting Session
+    // Storing Session
+    $user_check=$_SESSION['login_user'];
+    // SQL Query To Fetch Complete Information Of User
+    $query = "SELECT * FROM $table WHERE name='$user_check'";
+    $result = mysql_query($query);  
+
+    if($row = mysql_fetch_array($result)) {
+        $login_session = $row["name"];
+        $idRest = $row["idrest"];
+    }
+
+    if(!isset($login_session) || !isset($idRest) ){
+        header('Location: ../default.php'); // Redirecting To Home Page
+    }
+?>
--- Server/manager/storemenu.php
+++ Server/manager/storemenu.php
@@ -1,3 +1,6 @@
+<?php
+include './session.php';
+?>
 
 <?php
     $table = "menu";
