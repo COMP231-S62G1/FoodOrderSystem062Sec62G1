3c5282431645203a9c8ad2c3292eb707393c717d
Kathleen <kathleen.wilsonn@gmail.com>
2014-12-03 22:56:46 -0500

    select reports

--- Server/manager/storeleftreport.php
+++ Server/manager/storeleftreport.php
@@ -1,14 +1,14 @@
 <div id="left">
     <a href="#" class="auto-style1" onclick="document.storeinfo.submit();"><h3>Store Information</h3></a>
     <br />
-    <a href="#" class="auto-style1" onclick="document.storeaccount.submit();"><h3>Store account</h3></a>
+    <a href="#" class="auto-style1" onclick="document.storeaccount.submit();"><h3>Store Account</h3></a>
     <br />
-    <a href="#" class="auto-style1" onclick="document.storemenu.submit();"><h3>store menus</h3></a>
+    <a href="#" class="auto-style1" onclick="document.storemenu.submit();"><h3>Store Menus</h3></a>
     <br />	
-    <h3>store reports</h3>
+    <h3>Store Reports</h3>
     <ul>
         <li><ul>
-            <li><a href="#" class="auto-style1" onclick="document.storeorderreport.submit();">Order Report</a></li>
+			<li><a href="#" class="auto-style1" onclick="document.storeorderreport.submit();">Order Report</a></li>
             <li><a href="#" class="auto-style1" onclick="document.storesalesreport.submit();">Sales Report</a></li>
         </ul>
       </li>
